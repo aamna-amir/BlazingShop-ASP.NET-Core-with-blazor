6b6b7ff69003eb29bc184eeda050c9b6728c67b8
Amna Amir <aamna48@outlook.com>
2020-09-25 15:57:16 +0500

    add individual form validations

--- BlazingShop/BlazingShop/Data/Product.cs
+++ BlazingShop/BlazingShop/Data/Product.cs
@@ -12,9 +12,11 @@ namespace BlazingShop.Data
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
         [Range(0,500)]
         public double Price { get; set; }
         public byte[] Image { get; set; }
+        [Required]
         public string ShadeColor { get; set; }
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
