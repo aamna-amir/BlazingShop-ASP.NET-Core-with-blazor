d445064018623a98ba343825661c2c0916aa2409
Amna Amir <aamna48@outlook.com>
2020-09-27 21:59:47 +0500

    add ProductSize model

---
+++ BlazingShop/BlazingShop/Services/ProductSizeService.cs
@@ -0,0 +1,28 @@
+﻿using BlazingShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Services
+{
+    public class ProductSizeService
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductSizeService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ProductSize GetProductSize(int productSizeId)
+        {
+            ProductSize obj = new ProductSize();
+            return _db.ProductSize.FirstOrDefault(u => u.Id == productSizeId);
+        }
+
+        public List<ProductSize> GetProductsSizes()
+        {
+            return _db.ProductSize.ToList();
+        }
+    }
+}
